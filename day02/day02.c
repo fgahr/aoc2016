2af8bf7a3ed95fa399e7933c90d9fbf888cd62cf
Frederik Gahr <10232152+freag@users.noreply.github.com>
2019-03-24 02:30:08 +0100

    Day 2 & 3: Add solutions for both parts.

---
+++ day02/day02.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+#define SIMPLE_PAD_SIZE 3
+#define FANCY_PAD_SIZE 5
+#define NOP '\0'
+
+char enter_simple_pad(const char *command) {
+  /* Start at '5'. */
+  static int pos_x = 1;
+  static int pos_y = 1;
+  static int simple_pad[SIMPLE_PAD_SIZE][SIMPLE_PAD_SIZE] = {
+      {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+
+  char c;
+  for (int i = 0; (c = command[i]) != '\0' && c != '\n'; i++) {
+    switch (c) {
+    case 'U': {
+      pos_y = MAX(pos_y - 1, 0);
+      break;
+    }
+    case 'D': {
+      pos_y = MIN(pos_y + 1, SIMPLE_PAD_SIZE - 1);
+      break;
+    }
+    case 'L': {
+      pos_x = MAX(pos_x - 1, 0);
+      break;
+    }
+    case 'R': {
+      pos_x = MIN(pos_x + 1, SIMPLE_PAD_SIZE - 1);
+      break;
+    }
+    default:
+      fprintf(stderr, "Invalid command: %c\n", c);
+      exit(EXIT_FAILURE);
+      break;
+    }
+  }
+  return simple_pad[pos_y][pos_x];
+}
+
+char enter_fancy_pad(const char *command) {
+  /* Start at '5'. */
+  static int pos_x = 0;
+  static int pos_y = 2;
+  /* NOP marks fields which do not exist on the pad. */
+  static int fancy_pad[FANCY_PAD_SIZE][FANCY_PAD_SIZE] = {
+      {NOP, NOP, '1', NOP, NOP},
+      {NOP, '2', '3', '4', NOP},
+      {'5', '6', '7', '8', '9'},
+      {NOP, 'A', 'B', 'C', NOP},
+      {NOP, NOP, 'D', NOP, NOP}};
+
+  char c;
+  int newpos;
+  for (int i = 0; (c = command[i]) != '\0' && c != '\n'; i++) {
+    switch (c) {
+    case 'U': {
+      newpos = MAX(pos_y - 1, 0);
+      if (fancy_pad[newpos][pos_x] != NOP) {
+        pos_y = newpos;
+      }
+      break;
+    }
+    case 'D': {
+      newpos = MIN(pos_y + 1, FANCY_PAD_SIZE - 1);
+      if (fancy_pad[newpos][pos_x] != NOP) {
+        pos_y = newpos;
+      }
+      break;
+    }
+    case 'L': {
+      newpos = MAX(pos_x - 1, 0);
+      if (fancy_pad[pos_y][newpos] != NOP) {
+        pos_x = newpos;
+      }
+      break;
+    }
+    case 'R': {
+      newpos = MIN(pos_x + 1, FANCY_PAD_SIZE - 1);
+      if (fancy_pad[pos_y][newpos] != NOP) {
+        pos_x = newpos;
+      }
+      break;
+    }
+    default:
+      fprintf(stderr, "Invalid command: %c\n", c);
+      exit(EXIT_FAILURE);
+      break;
+    }
+  }
+  return fancy_pad[pos_y][pos_x];
+}
+
+void unlock(FILE *f, char (*enter_pad)(const char *)) {
+  char *lbuf;
+  size_t llen;
+  ssize_t nread;
+  while ((nread = getline(&lbuf, &llen, f)) != EOF) {
+    printf("%c", enter_pad(lbuf));
+  }
+  printf("\n");
+
+  free(lbuf);
+}
+
+void part_one(FILE *f) {
+  rewind(f);
+  unlock(f, &enter_simple_pad);
+}
+
+void part_two(FILE *f) {
+  rewind(f);
+  unlock(f, &enter_fancy_pad);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "%s -- Error: No input file given", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  FILE *f = fopen(argv[1], "r");
+  if (f == NULL) {
+    fprintf(stderr, "%s -- Error: File does not exist: %s", argv[0], argv[1]);
+    exit(EXIT_FAILURE);
+  }
+  char inbuf[BUFSIZ];
+  setbuf(f, inbuf);
+
+  part_one(f);
+  part_two(f);
+  fclose(f);
+  exit(EXIT_SUCCESS);
+}
---
+++ day03/day03.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int is_valid_triangle(int a, int b, int c) {
+  return a < b + c && b < a + c && c < a + b;
+}
+
+void part_one(FILE *f) {
+  rewind(f);
+  char *lbuf;
+  size_t llen;
+  ssize_t nread;
+  int num_triangles = 0;
+  int a, b, c;
+  while ((nread = getline(&lbuf, &llen, f)) != EOF) {
+    sscanf(lbuf, "%5d%5d%5d", &a, &b, &c);
+    num_triangles += is_valid_triangle(a, b, c);
+  }
+  printf("%d\n", num_triangles);
+
+  free(lbuf);
+}
+
+void part_two(FILE *f) {
+  rewind(f);
+  char *lbuf;
+  size_t llen;
+  int num_triangles = 0;
+  /* Read three triangles at once, hence store all sides at the same time. */
+  int a1, a2, a3;
+  int b1, b2, b3;
+  int c1, c2, c3;
+  /* Assume that the number of input lines is a multiple of 3. */
+  while (1) {
+    if (getline(&lbuf, &llen, f) == -1) {
+      break;
+    }
+
+    sscanf(lbuf, "%5d%5d%5d", &a1, &b1, &c1);
+    getline(&lbuf, &llen, f);
+    sscanf(lbuf, "%5d%5d%5d", &a2, &b2, &c2);
+    getline(&lbuf, &llen, f);
+    sscanf(lbuf, "%5d%5d%5d", &a3, &b3, &c3);
+    num_triangles += is_valid_triangle(a1, a2, a3) +
+                     is_valid_triangle(b1, b2, b3) +
+                     is_valid_triangle(c1, c2, c3);
+  }
+  printf("%d\n", num_triangles);
+
+  free(lbuf);
+}
+
+int main(int argc, const char *argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "%s -- Error: No input file given.", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  FILE *f = fopen(argv[1], "r");
+  if (f == NULL) {
+    fprintf(stderr, "%s -- Error: File does not exist: %s", argv[0], argv[1]);
+    exit(EXIT_FAILURE);
+  }
+  char inbuf[BUFSIZ];
+  setbuf(f, inbuf);
+
+  part_one(f);
+  part_two(f);
+
+  fclose(f);
+  exit(EXIT_SUCCESS);
+}
