8ff6432db468000642a5ab83c95734dbd0ef17b7
Frederik Gahr <10232152+freag@users.noreply.github.com>
2019-04-01 22:06:24 +0200

    Day 1: Cleanup.

--- day01/day01.c
+++ day01/day01.c
@@ -1,116 +1,134 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../lib/lib.h"
+
 #define ABS(x) ((x) > 0 ? (x) : -(x))
+/* Should be plenty for all inputs. */
+#define POSITION_LIST_SIZE 16384
 
-#define INFILE "input.txt"
+#define NORTH 0
+#define EAST 1
+#define SOUTH 2
+#define WEST 3
 
-void move(int *pos_x, int *pos_y, int dir, int distance) {
-  if (dir == 0) {
+/* Represents a direction along the path. */
+typedef int dir;
+
+/* Facing the given direction, move ahead from the x, y coordinates. */
+void move_ahead(int *pos_x, int *pos_y, dir direction, int distance) {
+  if (direction == NORTH) {
     *pos_y += distance;
-  } else if (dir == 1) {
+  } else if (direction == EAST) {
     *pos_x += distance;
-  } else if (dir == 2) {
+  } else if (direction == SOUTH) {
     *pos_y -= distance;
-  } else if (dir == 3) {
+  } else if (direction == WEST) {
     *pos_x -= distance;
   }
 }
 
-void part_one() {
-  FILE *f = fopen(INFILE, "r");
+int dist_from_start(int x, int y) { return ABS(x) + ABS(y); }
 
-  char c;
-  int distance;
+/* Facing in the given direction, adjust it to a right turn. */
+void turn_right(dir *direction) { *direction = (*direction + 1) % 4; }
 
-  int pos_x = 0;
-  int pos_y = 0;
-  int dir = 0;
+/* Facing in the given direction, adjust it to a left turn. */
+void turn_left(dir *direction) { *direction = (*direction + 3) % 4; }
 
-  while ((c = fgetc(f)) != EOF) {
-    if (c != 'R' && c != 'L') {
-      continue;
-    }
-    if (c == 'R') {
-      dir = (dir + 1) % 4;
-    } else {
-      dir = (dir + 3) % 4;
-    }
-    fscanf(f, "%d", &distance);
-    move(&pos_x, &pos_y, dir, distance);
-  }
-  fclose(f);
-
-  printf("Part one: %d\n", ABS(pos_x) + ABS(pos_y));
-}
-
-typedef struct place {
+/* Represents a place along the way that is being visited. */
+typedef struct {
   int x;
   int y;
-  struct place *next;
-} place;
+} location;
 
-place *new_place(int x, int y) {
-  place *p = malloc(sizeof(place));
-  p->x = x;
-  p->y = y;
-  p->next = NULL;
-  return p;
-}
-
-int seen(int pos_x, int pos_y, place *visited) {
-  if (visited == NULL) {
-    return 0;
-  }
-  if (pos_x == visited->x && pos_y == visited->y) {
-    return 1;
-  }
-  return seen(pos_x, pos_y, visited->next);
-}
-
-void part_two() {
-  FILE *f = fopen(INFILE, "r");
-
+/* Determine the distance from the start after following the instructions. */
+void part_one(FILE *f) {
+  rewind(f);
   char c;
   int distance;
 
   int pos_x = 0;
   int pos_y = 0;
-  int dir = 0;
-  place visited = {pos_x, pos_y, NULL};
-  place *current = &visited;
-  int skip_check = 1;
+  dir direction = NORTH;
 
   while ((c = fgetc(f)) != EOF) {
     if (c != 'R' && c != 'L') {
       continue;
     }
     if (c == 'R') {
-      dir = (dir + 1) % 4;
+      turn_right(&direction);
     } else {
-      dir = (dir + 3) % 4;
+      turn_left(&direction);
     }
     fscanf(f, "%d", &distance);
-    for (int i = 0; i < distance; i++) {
-      move(&pos_x, &pos_y, dir, 1);
-      if (skip_check) {
-        skip_check = 0;
-        continue;
-      }
-      if (seen(pos_x, pos_y, &visited)) {
-        fclose(f);
-        printf("Part two: %d\n", ABS(pos_x) + ABS(pos_y));
-        return;
-      }
-      current->next = new_place(pos_x, pos_y);
-      current = current->next;
-    }
+    move_ahead(&pos_x, &pos_y, direction, distance);
   }
-  fclose(f);
-  printf("Part two: %d\n", ABS(pos_x) + ABS(pos_y));
+
+  printf("%d\n", dist_from_start(pos_x, pos_y));
 }
 
-int main() {
-  part_one();
-  part_two();
+/* Determine the distance of the first self-intersection of the path. */
+void part_two(FILE *f) {
+  rewind(f);
+  char turn;
+  int distance;
+
+  int pos_x = 0;
+  int pos_y = 0;
+  int direction = 0;
+
+  /* Memorize all visited positions. */
+  location places[POSITION_LIST_SIZE];
+  places[0] = (location) {0, 0};
+  int place_idx = 1;
+
+  while (fscanf(f, "%c%d%*2[, ]", &turn, &distance) != EOF) {
+    switch (turn) {
+    case 'L': {
+      turn_left(&direction);
+      break;
+    }
+    case 'R': {
+      turn_right(&direction);
+      break;
+    }
+    default:
+      fprintf(stderr, "Unknown direction encountered: %c\n", turn);
+      exit(EXIT_FAILURE);
+      break;
+    }
+
+    for (int i = 0; i < distance; i++) {
+      move_ahead(&pos_x, &pos_y, direction, 1);
+      places[place_idx] = (location){pos_x, pos_y};
+      ++place_idx;
+      if (place_idx >= POSITION_LIST_SIZE) {
+        fprintf(stderr, "Position list size exceeded. Increase the limit or "
+                        "use dynamic re-allocation.");
+      }
+      /* Omit the current place. */
+      for (int j = 0; j < place_idx - 1; j++) {
+        if (pos_x == places[j].x && pos_y == places[j].y) {
+          printf("%d\n", dist_from_start(pos_x, pos_y));
+          return;
+        }
+      }
+    }
+  }
+
+  printf("%d\n", dist_from_start(pos_x, pos_y));
+}
+
+int main(int argc, const char *argv[]) {
+  FILE *f = read_input(argc, argv);
+  char buffer[BUFSIZ];
+  setbuf(f, buffer);
+
+  part_one(f);
+  part_two(f);
+
+  fclose(f);
+  exit(EXIT_SUCCESS);
 }
